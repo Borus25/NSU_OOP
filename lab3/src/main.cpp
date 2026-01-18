400ebb1c3d8383c78eaab75b934ba4108108cba5
Borus25 <maksimborisov51046@gmail.com>
2026-01-18 16:53:01 +0700

    update main

--- lab3/src/main.cpp
+++ lab3/src/main.cpp
@@ -1,9 +1,7 @@
 #include <iostream>
 #include "SoundProcessor/SoundProcessor.h"
-#include "tests/Tests.h"
 
 int main(int argc, char* argv[]) {
-    TestALL();
     try {
         SoundProcessor processor;
         return processor.run(argc, argv);
